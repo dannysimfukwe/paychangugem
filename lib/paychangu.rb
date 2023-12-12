57827d12cc73a00ff759e153f136a82274b903b9
Danny Simfukwe <dannysimfukwe@gmail.com>
2023-12-12 22:58:06 +0200

    chore: create payment link

--- lib/paychangu.rb
+++ lib/paychangu.rb
@@ -5,10 +5,57 @@ require 'net/http'
 require 'securerandom'
 
 require_relative "paychangu/version"
-url = URI("https://api.paychangu.com/payment").freeze
-
 
 module Paychangu
-  def createPaymentLink(options = {}, url)
+  class Payment
+    payment_url = URI("https://api.paychangu.com/").freeze
+    supported_currencies =  ['MWK', 'NGN', 'ZAR', 'GBP', 'USD', 'ZMW'].freeze
+
+    def initialize(secret_key)
+      @secret = setSecret(secret_key)
+      @url = payment_url
+    end
+
+    def createPaymentLink(data = {})
+      path = "payment"
+      http = Net::HTTP.new(@url.host, @url.port)
+      http.use_ssl = true
+
+      payload = {
+        :amount => data[:amount],
+        :currency => getSupportedCurrencies(data[:currency]),
+        :email => data[:email],
+        :first_name => data[:first_name],
+        :last_name => data[:last_name],
+        :callback_url => data[:callback_url],
+        :return_url => data[:return_url],
+        :tx_ref => data[:tx_ref] || SecureRandom.hex(10),
+        :customization => {
+            :title => data[:title],
+            :description => data[:description]
+            },
+        :logo => data[:logo]
+    }.to_json
+
+      request = Net::HTTP::Post.new(@url + "/#{path}")
+      request["accept"] = 'application/json'
+      request["Authorization"] =  "Bearer #{@secret}"
+      request["content-type"] = 'application/json'
+      request.body = payload
+
+      response = http.request(request)
+      response.read_body
+    end
+
+    def setSecret(secret_key)
+      raise "Secret key not provided!" unless secret_key
+      secret_key
+    end
+
+    def getSupportedCurrencies(currency) 
+      raise "currency list not provided!" unless supported_currencies.include?(currency)
+
+      currency
+    end
   end
 end
