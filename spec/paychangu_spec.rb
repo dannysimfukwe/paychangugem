c149011f6c910d019e4edd6fa47605567e898e7d
Danny Simfukwe <dannysimfukwe@gmail.com>
2023-12-13 11:05:55 +0200

    Chore: added tests

--- spec/paychangu_spec.rb
+++ spec/paychangu_spec.rb
@@ -1,11 +1,62 @@
 # frozen_string_literal: true
 
-RSpec.describe Paychangu do
-  it "has a version number" do
-    expect(Paychangu::VERSION).not_to be nil
+require "spec_helper"
+require "paychangu"
+
+RSpec.describe Paychangu::Payment do
+  let(:secret_key) { "Test" }
+
+  describe "#initialize" do
+    it "has a version number" do
+      expect(Paychangu::VERSION).not_to be nil
+    end
+    it "initializes the Payment class with a secret key" do
+      payment = described_class.new(secret_key)
+      expect(payment.instance_variable_get(:@secret)).to eq(secret_key)
+    end
+
+    it "raises an error if no secret key is provided" do
+      expect { described_class.new(nil) }.to raise_error(RuntimeError, "Secret key not provided!")
+    end
   end
 
-  xit "raises an error if a wrong currency is passed to it" do
-   
+  describe "#create_payment_link" do
+    it "creates a payment link" do
+      payment = described_class.new(secret_key)
+      data = {
+        amount: 100,
+        currency: "USD",
+        email: "test@example.com",
+        first_name: "John",
+        last_name: "Doe",
+        callback_url: "https://example.com/callback",
+        return_url: "https://example.com/return",
+        tx_ref: "txn_123456",
+        title: "Test Payment",
+        description: "This is a test payment",
+        logo: "https://example.com/logo.png"
+      }
+
+      expect { payment.create_payment_link(data) }.not_to raise_error
+    end
+
+    it "raises an error if the payment currency is not valid" do
+      payment = described_class.new(secret_key)
+      data = {
+        amount: 100,
+        currency: "USD1",
+        email: "test@example.com",
+        first_name: "John",
+        last_name: "Doe",
+        callback_url: "https://example.com/callback",
+        return_url: "https://example.com/return",
+        tx_ref: "txn_123456",
+        title: "Test Payment",
+        description: "This is a test payment",
+        logo: "https://example.com/logo.png"
+      }
+
+      expect { payment.create_payment_link(data) }.to raise_error(RuntimeError, "USD1 currency not supported!")
+    end
   end
 end
